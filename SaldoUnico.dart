bfe9c0471fe24ba2c91556c490ffe858f724bf78
Emerson Bernardino <emersonbernardino@Emersons-MacBook-Pro.local>
2012-04-22 13:53:48 -0300

    Data Callendar corrigido

--- SaldoUnico.dart
+++ SaldoUnico.dart
@@ -30,21 +30,8 @@ class SaldoUnico {
     
       <a href="http://www.google.com/calendar/event?action=TEMPLATE&text=Brunch at Java Cafe&dates=20060415T180000Z/20060415T190000Z&location=Java Cafe, San Francisco, CA&details=Try our Saturday brunch special:<br><br>French toast with fresh fruit<br><br>Yum!&trp=true&sprop= website:http://www.javacafebrunches.com&sprop;=name:Jave Cafe"><img src="http://www.google.com/calendar/images/ext/gc_button2.gif"></a>
     */
-    
-    var ano = date.year;
-    var mes = date.month;
-    var dia = date.day;
-    
-    if(mes < 10){
-      mes = "0$mes";
-    }
-    
-    if(dia < 10){
-      dia = "0$dia";
-    }
-    
-    //20060415T180000Z/20060415T190000Z    
-    String dateStr = "$ano$mes$dia"+"T050000Z/$ano$mes$dia"+"T060000Z";
+    String dateStr = Utils.formateDateFromTo(date);
+   
     
     return '<a target="_blank" href="http://www.google.com/calendar/event?action=TEMPLATE&text=$title&dates=$dateStr&details=$description&trp=true&sprop=name:SaldoUnico"><img src="http://www.google.com/calendar/images/ext/gc_button2.gif" ></a>';
   }
